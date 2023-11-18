4413dcf52618c51b49264a543ce50d93f20e701f
Op-Pautu <pautuop@gmail.com>
2023-11-18 14:06:25 +0530

    add sidebar and navitem inside it

---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Sidebar } from "../_components/sidebar";
+
+const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="max-w-6xl px-4 pt-20 mx-auto md:pt-24 2xl:max-w-screen-xl">
+      <div className="flex gap-x-7">
+        <div className="hidden w-64 shrink-0 md:block">
+          <Sidebar />
+        </div>
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default OrganizationLayout;
