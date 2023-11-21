189558921471b162db32a54dbe932314b0e207e4
Op-Pautu <pautuop@gmail.com>
2023-11-21 15:36:25 +0530

    add board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { HelpCircle, User2 } from "lucide-react";
 import React from "react";
@@ -10,21 +11,23 @@ export function BoardList() {
         Your boards
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-        <div
-          className="relative flex flex-col items-center justify-center w-full h-full transition rounded-sm aspect-video hover:opacity-75 bg-muted gap-y-1"
-          role="button"
-        >
-          <p className="text-sm">Create new board</p>
-          <span className="text-xs">5 remaining</span>
-          <Hint
-            sideOffset={40}
-            description={`
-                Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
-            `}
+        <FormPopover sideOffset={10} side={"right"}>
+          <div
+            className="relative flex flex-col items-center justify-center w-full h-full transition rounded-sm aspect-video hover:opacity-75 bg-muted gap-y-1"
+            role="button"
           >
-            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`
+            Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
+            `}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   );
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -2,6 +2,7 @@
 
 import {
   Popover,
+  PopoverClose,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
@@ -11,6 +12,9 @@ import { createBoard } from "@/actions/create-board";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { Button } from "../ui/button";
+import { X } from "lucide-react";
+import { toast } from "sonner";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -19,4 +23,61 @@ interface FormPopoverProps {
   sideOffset?: number;
 }
 
-export const FormPopover = ({} = {});
+export const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+      toast.success("Board created!");
+    },
+    onError: (error) => {
+      console.log({ error });
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="pt-3 w-80"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="pb-4 text-sm font-medium text-center text-neutral-600">
+          Create Board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="absolute w-auto h-auto p-2 top-2 right-2 text-neutral-600"
+            variant={"ghost"}
+          >
+            <X className="w-4 h-4" />
+          </Button>
+        </PopoverClose>
+        <form className="space-y-4" action={onSubmit}>
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
