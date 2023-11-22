093ca7726b5a74af240824a0757f2e32cca9f835
Op-Pautu <pautuop@gmail.com>
2023-11-22 11:25:10 +0530

    fetch images using unsplash

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -15,6 +15,7 @@ import { FormSubmit } from "./form-submit";
 import { Button } from "../ui/button";
 import { X } from "lucide-react";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -68,6 +69,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form className="space-y-4" action={onSubmit}>
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
