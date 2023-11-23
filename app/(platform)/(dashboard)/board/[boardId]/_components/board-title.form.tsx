3db8a3713aa71ca06149518ef793f3822301589e
Op-Pautu <pautuop@gmail.com>
2023-11-23 17:31:20 +0530

    add update and delete actions for boards

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title.form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title.form.tsx
@@ -1,18 +1,33 @@
 "use client";
 
+import { updateBoard } from "@/actions/update-board";
 import { FormInput } from "@/components/form/form-input";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
 import { Board } from "@prisma/client";
 import { ElementRef, useRef, useState } from "react";
+import { toast } from "sonner";
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export function BoardTitleForm({ data }: BoardTitleFormProps) {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated!`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
 
+  const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
   const enableEditing = () => {
@@ -23,13 +38,17 @@ export function BoardTitleForm({ data }: BoardTitleFormProps) {
       inputRef.current?.select();
     });
   };
+
   const disableEditing = () => {
     setIsEditing(false);
   };
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
-    console.log("submit");
+    execute({
+      title,
+      id: data.id,
+    });
   };
 
   const onBlur = () => {
@@ -47,7 +66,7 @@ export function BoardTitleForm({ data }: BoardTitleFormProps) {
           ref={inputRef}
           id="title"
           onBlur={onBlur}
-          defaultValue={data.title}
+          defaultValue={title}
           className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible: outline-none focus-visible:ring-transparent border-none"
         />
       </form>
@@ -60,7 +79,7 @@ export function BoardTitleForm({ data }: BoardTitleFormProps) {
       variant={"transparent"}
       className="font-bold text-lg h-auto w-auto p-1 px-2"
     >
-      {data.title}
+      {title}
     </Button>
   );
 }
