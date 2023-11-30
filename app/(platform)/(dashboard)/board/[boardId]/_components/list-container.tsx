b1eceaabca51c4babcef95e63ad10e5bf0dc5ec3
Op-Pautu <pautuop@gmail.com>
2023-11-30 13:41:37 +0530

    implement drag and drop UI

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
-import { useEffect, useState } from "react";
 import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
+}
+
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  // Create a shallow copy of the original array
+  const result = Array.from(list);
+
+  // Remove the element at the startIndex from the copied array and store it in 'removed'
+  const [removed] = result.splice(startIndex, 1);
+
+  // Insert the 'removed' element at the endIndex in the copied array
+  result.splice(endIndex, 0, removed);
+
+  // Return the new array with reordered elements
+  return result;
 }
 
 export function ListContainer({ boardId, data }: ListContainerProps) {
@@ -17,13 +33,54 @@ export function ListContainer({ boardId, data }: ListContainerProps) {
     setOrderedData(data);
   }, [data]);
 
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    // Dropped in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // User moves a list
+    if (type === "list") {
+      const items = reorder(orderedData, source.index, destination.index).map(
+        (item, index) => ({
+          ...item,
+          order: index,
+        })
+      );
+
+      setOrderedData(items);
+
+      // TODO: Trigger Server Action
+    }
+  };
+
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => {
-        return <ListItem key={list.id} index={index} data={list} />;
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} index={index} data={list} />;
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
