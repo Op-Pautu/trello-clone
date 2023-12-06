02d03cf10f98498ca5630a1bccfa999b7514659e
Op-Pautu <pautuop@gmail.com>
2023-12-06 17:18:00 +0530

    update UI and actions for pro users

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -6,7 +6,11 @@ import Image from "next/image";
 import { CreditCard } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 
-export function Info() {
+interface InfoProps {
+  isPro: boolean;
+}
+
+export function Info({ isPro }: InfoProps) {
   const { organization, isLoaded } = useOrganization();
 
   if (!isLoaded) {
@@ -28,7 +32,7 @@ export function Info() {
         <p className="font-semibold text-xl">{organization?.name}</p>
         <div className="flex items-center text-xs text-muted-foreground">
           <CreditCard className="h-3 w-3 mr-1" />
-          Free
+          {isPro ? "Pro" : "Free"}
         </div>
       </div>
     </div>
